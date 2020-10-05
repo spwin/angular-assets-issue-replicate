cef3009a54cb0f2a32e9c510432164fd1aacd5a7
Stanislav Markevic <smarkevic@freelancer.com>
2020-10-05 12:20:05 +0100

    replicate the issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <div>
+      <h2>Assets issue</h2>
+      <pre>Running ng serve and ng build renders different image below.</pre>
+      <img src="../assets/face.png" alt="face"/>
+    </div>
+  `,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
